44cce54ab964adc161564eb6941d2b689fd99f02
AluHnor <uchihamagatsu@hotmail.com>
2025-04-03 13:58:42 -0300

    estruturas de repetição ok

---
+++ Estruturas/src/ExemploArray.java
@@ -0,0 +1,9 @@
+public class ExemploArray {
+    public static void main(String[] args) {
+        String alunos [] = {"NEKO" , "SILVANA" , "ANYSSA" , "HYLDA"};
+        for (int x=0; x < alunos.length; x++){
+
+            System.out.println("O aluno no índice x=" + x + " é " + alunos [x]);
+        }
+    }
+}
---
+++ Estruturas/src/ExemploEach.java
@@ -0,0 +1,10 @@
+public class ExemploEach {
+    public static void main(String[] args) {
+        String alunos [] = {"NEKO" , "SILVANA" , "ANYSSA" , "HYLDA"};
+
+        for (String aluno : alunos){
+
+            System.out.println("Nome do aluno é " + aluno);
+        }
+    }
+}
